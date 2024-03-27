93dc19f32fe6135ce3c2ad4d4982e22b285de83a
Xocash695 <akash.kallumkal@gmail.com>
2024-03-26 23:07:15 -0400

    added  test view for the login

---
+++ Someapp/login.swift
@@ -0,0 +1,18 @@
+//
+//  login.swift
+//  Someapp
+//
+//  Created by Akash Kallumkal on 2024-03-24.
+//
+
+import SwiftUI
+
+struct login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    login()
+}
---
+++ Someapp/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  Someapp
+//
+//  Created by Akash Kallumkal on 2024-03-26.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
